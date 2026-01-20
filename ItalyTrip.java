54e56872171098745810daf0f27e7eb83332df7e
Aaniyah <aaniyah.ellis2027@gbwl.org>
2026-01-20 16:06:54 +0000

    done for 1/20

---
+++ ItalyTrip.java
@@ -0,0 +1,19 @@
+public class ItalyTrip{
+    //INSTANCE VARIABLES
+    private String coastType;
+    private int processingFee;
+    private boolean ifNeedPaymentPlan;
+    private int durationOfTrip;
+    private boolean isLuxury;
+    private boolean isOutdoor;
+    private boolean isExploratory;
+    private double totalCost;
+
+public 
+
+
+
+
+
+
+}//end class
