7afff1ff3b2b5eb7fe04b2e6ffbc41f7922cc616
Aaniyah <aaniyah.ellis2027@gbwl.org>
2026-04-24 13:40:40 +0000

    worked on getters need to finish setters

--- ItalyTrip.java
+++ ItalyTrip.java
@@ -1,38 +1,65 @@
 public class ItalyTrip{
     //INSTANCE VARIABLES
     private String coastType;
-    private int processingFee;
     private boolean ifNeedPaymentPlan;
     private int durationOfTrip;
     private boolean isLuxury;
     private boolean isOutdoorsy;
     private boolean isExploratory;
     private double totalCost;
+    private static int processingFee = 55; 
 
 public ItalyTrip (){
     this.coastType = "Amalfi Coast";
-    this.processingFee = 50;
     this.ifNeedPaymentPlan = true; 
     this.durationOfTrip = 7;
     this.isLuxury = true;
     this.isOutdoorsy = false;
     this.isExploratory = false;
-    this.totalCost = 1500.60;
+    this.totalCost = durationOfTrip * processingFee;
 }
 
 public ItalyTrip (String ct, int n, boolean t , int d, boolean lt , boolean ot, boolean et) {
     this.coastType = ct;
-    this.processingFee = n;
     this.ifNeedPaymentPlan = t; 
     this.durationOfTrip = n;
     this.isLuxury = lt;
     this.isOutdoorsy = ot;
     this.isExploratory = et;
-    this.totalCost = 1500.60;
+    this.totalCost = durationOfTrip * processingFee;
 }
 
+//getters
+public String getCoastType(){
+    return this.coastType;
+}
+public boolean getIfNeedPaymentPlan(){
+    return this.ifNeedPaymentPlan;
+}
+public int getDurationOfTrip(){
+    return this.durationOfTrip;
+}
+public boolean getIsLuxury(){
+    return this.isLuxury;
+}
+public boolean getIsOutdoorsy(){
+    return this.isOutdoorsy;
+}
+public boolean getIsExploratory(){
+    return this.isExploratory;
+}
+public double getTotalCost(){
+    return this.totalCost;
+}
 
+//setters
+public void setCoastType (String new c){
+    this.coastType = new c;
+}
 
+public void setIfNeedPaymentPlan (boolean new i){
+    this.
+}
 
 
 
--- Main.java
+++ Main.java
@@ -1,7 +1,43 @@
+import java.util.Scanner;
+
 public class Main {
 
    public static void main(String []args) {
-      ItalyTrip t1 = new ItalyTrip("Cilento Coast", 50 , true, false, true);
+      ItalyTrip trip1 = new ItalyTrip("Cilento Coast", 50 , true, false, true);
       System.out.println(t1);
+   }
+   // below the main method but inside Main class
+   // method to create objects
+   public static ItalyTrip bookTrip (Scanner scan){
+      ItalyTrip trip1 = new ItalyTrip ();
+      System.out.println("What is your prefered coast type? ");
+      String coastType = scan.nextLine();
+      System.out.println("Do you want a payment plan? true/false ");
+      boolean ifNeedPaymentPlan = scan.nextBoolean();
+      scan.nextLine();
+      System.out.println("How long would your travel be? ");
+      int durationOfTrip = scan.nextInt();
+      scan.nextLine();
+      System.out.println("Do you want a luxury vaca? true/false ");
+      boolean isLuxury = scan.nextBoolean();
+      scan.nextLine();
+
+      System.out.println("Do you want an Outdoors? true/false ");
+      boolean isOutdoorsy = scan.nextBoolean();
+      scan.nextLine();
+
+      System.out.println("Do you want to explore? true/false ");
+      boolean isExploratory = scan.nextBoolean(); 
+      scan.nextLine();
+
+      trip1.setCoastType(coastType);
+      trip1.setIfNeedPaymentPlan(ifNeedPaymentPlan);
+      trip1.setDurationOfTrip(durationOfTrip);
+      trip1.setIsLuxury(isLuxury);
+      trip1.setIsOutdoorsy(isOutdoorsy);
+      trip.setIsExploratory(isExploratory);
+      trip1.setTotalCost();
+      return trip1;
+
    }
 }
